9e7ac673ca00109e452711332c686905eec72762
Murad Muradli <mmuradmuradlii@gmail.com>
2023-12-22 21:55:36 +0100

    Create show note modal

--- src/components/ShowNote.jsx
+++ src/components/ShowNote.jsx
@@ -1,22 +1,34 @@
 import React from "react";
 import "../css/shownote.css";
 import { IoCloseSharp } from "react-icons/io5";
+import { AnimatePresence, motion } from "framer-motion";
 
-const ShowNote = ({showNote,toggleNote}) => {
-  return (
-    <>
-      <div
-        className={
-          showNote ? "shownote-container show-note" : "shownote-container"
-        }
-      >aaa</div>
-      <div className="content">
-        <button type="button" className="close-btn" onClick={toggleNote}>
-        <IoCloseSharp />
-        </button>
-      </div>
-    </>
-  );
+const ShowNote = ({ showNote, setShowNote }) => {
+	return (
+		<AnimatePresence>
+			{showNote && (
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					className={`shownote-container`}
+				>
+					<div className="shownote-content">
+						<div className="close-btn-container">
+							<button
+								type="button"
+								className="close-btn"
+								onClick={() => setShowNote(false)}
+							>
+								<IoCloseSharp size={25} />
+							</button>
+						</div>
+						<input />
+					</div>
+				</motion.div>
+			)}
+		</AnimatePresence>
+	);
 };
 
 export default ShowNote;
--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -4,55 +4,52 @@ import "../css/note.css";
 import { TfiNotepad } from "react-icons/tfi";
 import ShowNote from "../components/ShowNote";
 
-
 const Note = () => {
-  const [showNote, setNote] = useState(false);
+	const [showNote, setShowNote] = useState(false);
 
-  const toggleNote = () => {
-    setNote(!showNote);
-  };
+	const toggleNote = () => {
+		setShowNote(!showNote);
+	};
 
-  return (
-    <div className="main-note">
-      <ShowNote showNote={showNote} toggleNote={toggleNote}>
-      </ShowNote>
-      <div className="up-container">
-        <div className="up-components">
-          <TfiNotepad></TfiNotepad>
-        </div>
-      </div>
-      <div className="note-container">
-        <div className="note-components">
-          <div className="note-components-left">
-            <p
-              style={{
-                color: "beige",
-                fontFamily: "Barlow Condensed",
-                display: "flex",
-                justifyContent: "center",
-              }}
-            >
-              Daily quotes:
-            </p>
-            <p style={{ color: "beige", fontFamily: "Bebas Neue" }}>
-              "Life is a journey with its fair share of ups and downs, twists
-              and turns. It's not always about the destination, but the
-              experiences along the way. Challenges may arise, but our
-              responses to them define the chapters of our story. Embrace the
-              moments, learn from the hardships, and cherish the joys, for in
-              the end, it's not the years in your life that count, but the
-              life in your years."
-            </p>
-            <button className="create-note" onClick={toggleNote}>
-              Create Note
-            </button>
-          </div>
-          <div className="note-components-right"></div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="main-note">
+			<ShowNote showNote={showNote} setShowNote={setShowNote}></ShowNote>
+			<div className="up-container">
+				<div className="up-components">
+					<TfiNotepad></TfiNotepad>
+				</div>
+			</div>
+			<div className="note-container">
+				<div className="note-components">
+					<div className="note-components-left">
+						<p
+							style={{
+								color: "beige",
+								fontFamily: "Barlow Condensed",
+								display: "flex",
+								justifyContent: "center",
+							}}
+						>
+							Daily quotes:
+						</p>
+						<p style={{ color: "beige", fontFamily: "Bebas Neue" }}>
+							"Life is a journey with its fair share of ups and downs, twists
+							and turns. It's not always about the destination, but the
+							experiences along the way. Challenges may arise, but our responses
+							to them define the chapters of our story. Embrace the moments,
+							learn from the hardships, and cherish the joys, for in the end,
+							it's not the years in your life that count, but the life in your
+							years."
+						</p>
+						<button className="create-note" onClick={toggleNote}>
+							Create Note
+						</button>
+					</div>
+					<div className="note-components-right"></div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Note;
-  
